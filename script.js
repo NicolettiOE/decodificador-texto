0d24f2bd330336156d503b099c5ab07c0f4c88a8
NicolettiOE <rafaelaoe.ads@gmail.com>
2024-08-22 19:05:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+const textArea = document.querySelector(".text-area");
+const mensagem = document.querySelector(".mensagem");
+const btnCopiar = document.getElementById("btn-copiar");
+
+//As "chaves" de criptografia que utilizaremos são:
+//A letra "e" é convertida para "enter"
+//A letra "i" é convertida para "imes"
+//A letra "a" é convertida para "ai"
+//A letra "o" é convertida para "ober"
+//A letra "u" é convertida para "ufat"
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(textArea.value);
+    mensagem.value = textoEncriptado;
+    textArea.value = "";
+}
+
+function encriptar(stringEncriptada) { 
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+
+    return stringEncriptada;
+}
+
+function btnDesencriptar() {
+    const textoDesencriptado = desencriptar(textArea.value);
+    mensagem.value = textoDesencriptado;
+    textArea.value = "";
+}
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+
+    return stringDesencriptada;
+}
+
+// Função para copiar o texto
+function copiarTexto() {
+    navigator.clipboard.writeText(mensagem.value)
+        .then(() => {
+            console.log('Texto copiado com sucesso!');
+        })
+        .catch(err => {
+            console.error('Erro ao copiar o texto: ', err);
+        });
+}
+
+// Adiciona o evento de clique ao botão "Copiar"
+btnCopiar.addEventListener("click", copiarTexto);
